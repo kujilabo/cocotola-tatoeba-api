500617ab42c9707696ed90f83a8e6bb171fc2530
pecolynx <pecolynx@gmail.com>
2022-04-17 23:38:38 +0900

    open telemetry

--- pkg_lib/gateway/db_mysql.go
+++ pkg_lib/gateway/db_mysql.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4/database"
+	migrate_mysql "github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	gorm_logrus "github.com/onrik/gorm-logrus"
-	"gorm.io/gorm"
-
-	migrate_mysql "github.com/golang-migrate/migrate/v4/database/mysql"
 	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func OpenMySQL(username, password, host string, port int, database string) (*gorm.DB, error) {
